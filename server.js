b66e27d3e72a4c5b840f26b0f6ed63d951845929
mychal <mychal9251@gmail.com>
2021-04-17 17:35:29 -0700

    add employee function is now working

--- server.js
+++ server.js
@@ -15,30 +15,35 @@ const connection = mysql.createConnection({
     password: 'Rooster1',
     database: "employee_db",
 });
-const init = async() =>{
+const init = async () => {
     await connection.connect((err) => {
         if (err) throw err;
-        
-        
-    });
-    await getEmployee()
-}
-init()
 
-const displayFig = async() => {
-    figlet('Scranton!!', function(err, data) {
+
+    });
+    console.log("something in here")
+    await displayFig()
+
+
+}
+
+
+const displayFig = () => {
+    figlet('Scranton!!', function (err, data) {
         if (err) {
             console.log('Something went wrong...');
             console.dir(err);
             return;
         }
         console.log(data)
+        getEmployee()
     });
 }
 
-const getEmployee = async() => {
-    await displayFig()
-   await inquirer.prompt({
+const getEmployee = async () => {
+    console.log("emplyee func")
+
+    return inquirer.prompt({
         name: "action",
         type: "rawlist",
         message: "what are you wanting to do",
@@ -66,13 +71,13 @@ const getEmployee = async() => {
                     break;
 
                 case 'Add Department':
-                addDepartment();
+                    addDepartment();
                     break;
 
                 case 'View Department List':
                     viewDepartment();
                     break;
-                
+
                 case 'Add Roles':
                     addRoles();
                     break;
@@ -84,7 +89,7 @@ const getEmployee = async() => {
                 case 'Destroy Employee':
                     deleteEmployee();
                     break;
-                
+
                 case 'View Employee by Manager':
                     viewByManager()
                     break;
@@ -95,44 +100,90 @@ const getEmployee = async() => {
 
 viewEmployee = () => {
     connection.query('SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN roles r ON e.role_id = r.department_id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC', (err, res) => {
-      if (err) throw err;
-    console.table(res)
-    getEmployee()
-    });
-  };
-
-  viewDepartment = () => {
-    connection.query("SELECT * FROM department", (err, res) => {
-      if (err) throw err;
-      console.table(res);
-      getEmployee();
-    });
-  };
-  addDepartment = () => {
-    inquirer.prompt([
-      {
-        name: "department",
-        type: "input",
-        message: " new department?"
-      }
-    ]).then(function(answerDept) {
-      connection.query(`INSERT INTO department (name) VALUES ('${answerDept.department}')`, (err, res) => {
         if (err) throw err;
-        viewDepartment();
+        console.table(res)
+        getEmployee()
+    });
+};
+
+viewDepartment = () => {
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        console.table(res);
         getEmployee();
-      }) 
+    });
+};
+
+
+addDepartment = () => {
+    inquirer.prompt([
+        {
+            name: "department",
+            type: "input",
+            message: " new department?"
+        }
+    ]).then(function (answerDept) {
+        connection.query(`INSERT INTO department (name) VALUES ('${answerDept.department}')`, (err, res) => {
+            if (err) throw err;
+            viewDepartment();
+            getEmployee();
+        })
     })
-  };
+};
 
-  viewRole = () => {
-      connection.query("SELECT * FROM employee_db.roles;", (err, res) => {
-          if (err) throw err;
-          console.table(res);
-          getEmployee();
+viewRole = () => {
+    connection.query("SELECT * FROM employee_db.roles;", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        getEmployee();
 
-      })
-  }
+    })
+}
 
 addEmployee = () => {
-    
-} 
+    inquirer.prompt([{
+
+        name: "first_name",
+        type: "input",
+        message: "Employee's first name?",
+
+    },
+    {
+        name: "last_name",
+        type: "input",
+        message: "Employee's Last name?",
+
+    },
+    {
+        name: "role_id",
+        type: "list",
+        message: "What role? Select a Number 1 for Manager, 2 Sales, 3 Customer Care, 4 Warehouse",
+        choices: ["1", "2", "3", "4"]
+
+    },
+    {
+        name: "id",
+        type:"input",
+        message: "what is your 2 digit id number?", 
+    },
+    {
+        name: "manager_id",
+        type: "list",
+        message: "Where is this employee working? 2 for Ofiice, 9 for warehouse",
+        choices: ["2", "9"]
+
+    }
+    ])
+    .then(function(answerEmpl) {
+        connection.query(`INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES("${answerEmpl.id}", "${answerEmpl.first_name}", "${answerEmpl.last_name}", "${answerEmpl.role_id}", "${answerEmpl.manager_id}")` ,(err,res)=>{
+            if (err) throw err;
+            console.log("added employee")
+            viewEmployee()
+        } 
+        )
+    })
+
+
+}
+
+init()
